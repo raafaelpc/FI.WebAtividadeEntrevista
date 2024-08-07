0a3d83a0318f3c0371fd68b4b3813adfa32bfc62
Rafael Patricio <rafael29pc@hotmail.com>
2024-08-07 09:20:11 -0300

    Atualizando db

--- FI.WebAtividadeEntrevista/App_Start/BundleConfig.cs
+++ FI.WebAtividadeEntrevista/App_Start/BundleConfig.cs
@@ -38,12 +38,6 @@ namespace FI.WebAtividadeEntrevista
             bundles.Add(new ScriptBundle("~/bundles/altClientes").Include(
                       "~/Scripts/Clientes/FI.AltClientes.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/beneficiarios").Include(
-                      "~/Scripts/Beneficiarios/FI.Beneficiarios.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/altBeneficiarios").Include(
-                      "~/Scripts/Beneficiarios/FI.AltBeneficiarios.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
